3d9aaf380c01e72e6ea1f201146e1c2ace3fac29
KumarUniverse <AK887199@wcupa.edu>
2019-11-14 12:43:53 -0500

    make project files.

---
+++ grsh.c
@@ -0,0 +1,35 @@
+/*
+ * Filename: my_wc.c
+ * Author: Akash Kumar
+ * Class: CSC 331
+ * Date: 11/15/2019
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  int interactive = 0;
+
+  // if no arguments are passed to the program, set to
+  // interactive mode. Else set to batch mode.
+  if (argc < 2)
+  {
+    iteractive = 1;
+  }
+
+  if (interactive)
+  {
+
+  }
+  else
+  {
+
+  }
+
+  return 0;
+}
